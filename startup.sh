30c44cbd3b1eb577c7295bc12434f350941ce8cc
Mike Zrimsek <mikezrimsek@gmail.com>
2016-10-11 02:13:03 -0400

    update startup script

--- startup.sh
+++ startup.sh
@@ -13,8 +13,6 @@ echo "Building React dependencies"
 npm install
 echo "Building JS and CSS bundles"
 webpack
-
 echo "Project build complete"
-echo "run 'dotnet run' in UI project to view in browser"
 
 echo "Project ready...run 'dotnet run' in UI project to launch"

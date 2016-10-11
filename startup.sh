d566130cd8c183af667eb89c45eb51b0bb2833e7
Mike Zrimsek <mikezrimsek@gmail.com>
2016-10-11 02:08:24 -0400

    add react build stuff to script

--- startup.sh
+++ startup.sh
@@ -8,4 +8,13 @@ dotnet restore
 cd ../UI
 dotnet restore
 echo "All dependencies built"
+
+echo "Building React dependencies"
+npm install
+echo "Building JS and CSS bundles"
+webpack
+
+echo "Project build complete"
+echo "run 'dotnet run' in UI project to view in browser"
+
 echo "Project ready...run 'dotnet run' in UI project to launch"
